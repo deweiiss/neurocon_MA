b59f83b2f9aeae8592ec736d59f26f9e2790ec44
Julian Weiß <julian.weiss94@live.de>
2024-10-07 11:14:42 +0200

    docker compose setup for user_mgmt and mongo

--- user_mgmt/util/db.py
+++ user_mgmt/util/db.py
@@ -10,6 +10,10 @@ class MongoConnector:
         self.app.config['MONGO_URI'] = "mongodb://localhost:27017/NEUROCON"
         mongo_client = PyMongo(self.app)
         db = mongo_client.db
+        # on first startup insert document
+        if 'sampleCollection' not in db.collection_names():
+            db.sampleCollection.insert_one({"name": "Initial Document"})
+
         # TODO: enable scheme check once they are updated
         # self.checkSchemes(db)
         return db
